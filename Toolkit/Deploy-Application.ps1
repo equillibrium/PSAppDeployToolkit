8d30e5bfe78038bed78f727d7178573c68dbfe54
Мыльцев Александр Михайлович <myltsev_a@tass.ru>
2023-02-01 14:05:55 +0300

    Update Deploy-Application.ps1

--- Toolkit/Deploy-Application.ps1
+++ Toolkit/Deploy-Application.ps1
@@ -227,7 +227,7 @@ Try {
 
         ## <Perform Installation tasks here>
         if ($TASS_IsChoco) {
-            Execute-Process -Path "choco" -Parameters ("install $TASSChocoAppName "+$(if ($TASSChocoPackageParams){"--params `"$TASSChocoPackageParams`" "})+"-s=TASS -log-file=$($configToolkitLogDir+ "\$TASSChocoAppName`_chocoInstall.log") -y") -PassThru -Verbose -CreateNoWindow
+            Execute-Process -Path "choco" -Parameters ("upgrade $TASSChocoAppName "+$(if ($TASSChocoPackageParams){"--params `"$TASSChocoPackageParams`" "})+"-s=TASS -log-file=$($configToolkitLogDir+ "\$TASSChocoAppName`_chocoInstall.log") -y") -PassThru -Verbose -CreateNoWindow
         }
 
         ##*===============================================
